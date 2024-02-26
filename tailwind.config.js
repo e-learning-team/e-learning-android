b4d1a73fb380673e4188236218140d063ef90e80
thomsonbel12 <thomsonbel12@gmail.com>
2024-02-27 01:00:19 +0700

    ++upcode fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
+// /** @type {import('tailwindcss').Config} */
+// import { glob, globSync } from 'glob' 
+// module.exports = {
+//   content: [ "./App.tsx", ...glob.sync('./src/**/.tsx'), ],
+//   // content: [
+//   //   "./App.tsx",
+//   //   // "./src/**/*.{js,jsx,ts,tsx}",
+//   //   "./src/screens/Home.tsx",
+//   //   "./src/screens/MainLayout.tsx",
+//   //   "./src/screens/Profile.tsx",
+//   //   "./src/screens/Search.tsx",
+//   //   "./src/screens/SplashScreen.tsx",
+//   // ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
+const globSync = require('glob').sync;
+
 module.exports = {
   content: [
     "./App.tsx",
-    // "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/screens/Home.tsx",
-    "./src/screens/MainLayout.tsx",
-    "./src/screens/Profile.tsx",
-    "./src/screens/Search.tsx",
-    "./src/screens/SplashScreen.tsx",
+    ...globSync('./src/**/*.tsx'),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
